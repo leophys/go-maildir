00e2ba5c105b349774030d3c4b0236175e44bcbc
Blallo <blallo@autistici.org>
2024-10-15 23:23:19 +0200

    Expose internal/ as type aliases

--- internal/maildir_notwindows.go
+++ internal/maildir_notwindows.go
@@ -1,8 +1,8 @@
-//go:build windows
+//go:build !windows
 
-package maildir
+package internal
 
 // The separator separates a messages unique key from its flags in the filename.
 // This should only be changed on operating systems where the colon isn't
 // allowed in filenames.
-const separator rune = ';'
+const separator rune = ':'
--- internal/maildir_test.go
+++ internal/maildir_test.go
@@ -1,4 +1,4 @@
-package maildir
+package internal
 
 import (
 	"errors"
